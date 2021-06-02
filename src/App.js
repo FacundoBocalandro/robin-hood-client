222f6f7114c29d4ead825d73d541c968939450ff
Facundo Bocalandro <facundo@bocalandro.com.ar>
2021-06-02 01:31:02 -0300

    companies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './main.css';
-import {Switch, Route, BrowserRouter, Redirect} from "react-router-dom";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import LoginContainer from "./session/login/LoginContainer";
 import RegisterContainer from "./session/register/RegisterContainer";
+import Companies from "./companies/Companies";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
                 <Switch style={{width: '100%', height: '100%'}}>
                     <Route exact path={"/"} component={LoginContainer}/>
                     <Route path={"/register"} component={RegisterContainer}/>
+                    <Route path={"/main/companies"} component={Companies}/>
                 </Switch>
             </div>
         </BrowserRouter>
---
+++ src/companies/Companies.js
@@ -0,0 +1,75 @@
+import React, {useEffect, useState} from "react";
+import {InputAdornment, TextField} from "@material-ui/core";
+import {get} from "../utils/http";
+import "./Companies.css";
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import {Search} from "@material-ui/icons";
+import Button from "@material-ui/core/Button";
+
+const companiesMock = [
+    ...Array(5).fill(0).map(() => ({name: "Apple", ticker: "AAPL", price: 100.0, up: (Math.random() < 0.5), percentage: Math.random().toFixed(2)})),
+    ...Array(5).fill(0).map(() => ({name: "Tesla", ticker: "TSLA", price: 200.2, up: (Math.random() < 0.5), percentage: Math.random().toFixed(2)})),
+    ...Array(5).fill(0).map(() => ({name: "Microsoft", ticker: "MSFT", price: 50.01, up: (Math.random() < 0.5), percentage: Math.random().toFixed(2)})),
+    ...Array(5).fill(0).map(() => ({name: "NVIDIA", ticker: "NVDA", price: 1000.5, up: (Math.random() < 0.5), percentage: Math.random().toFixed(2)})),
+    ...Array(5).fill(0).map(() => ({name: "Sirius", ticker: "SIR", price: 10000, up: true, percentage: 100})),
+].sort(() => 0.5 - Math.random());
+
+const Companies = () => {
+    const [search, setSearch] = useState("");
+    const [companies, setCompanies] = useState([...companiesMock]);
+
+    useEffect(() => {
+        get('companies')
+            .then(res => setCompanies(res))
+            .catch(err => {
+            })
+    }, [])
+
+    const includesSearch = (value) => {
+        return value.name.toLowerCase().includes(search.toLowerCase()) || value.ticker.toLowerCase().includes(search.toLowerCase())
+    }
+
+    const filteredCompanies = companies.filter(includesSearch);
+
+    return (
+        <div className={"companies-screen"}>
+            <TextField className={"companies-search-input"}
+                       InputProps={{
+                           startAdornment: (
+                               <InputAdornment position="start">
+                                   <Search />
+                               </InputAdornment>
+                           ),
+                       }}
+                       value={search}
+                       onChange={e => setSearch(e.target.value)}
+                       placeholder={"Search by company name or ticker..."}
+            />
+            <div className={"companies-list"}>
+                {filteredCompanies.length > 0 ? filteredCompanies.map(company => (
+                    <div className={"company-card"}>
+                        <div className={"company-card-header"}>
+                            <span className={"company-ticker"}>{company.ticker}</span>
+                            <span className={"company-name"}>{company.name}</span>
+                        </div>
+                        <div className={"company-card-price"}>
+                            <span className={"company-price"}>${company.price}</span>
+                            <div className={`company-card-percentage${company.up > 0 ? ' price-up' : ' price-down'}`}>
+                                <span className={"company-percentage"}>% {company.percentage}</span>
+                                {company.up ? <ArrowUpwardIcon className={"arrow-icon"}/> : <ArrowDownwardIcon className={"arrow-icon"}/>}
+                            </div>
+                        </div>
+                        <Button color={"default"} variant={"contained"}>Comprar</Button>
+                    </div>
+                )) :
+                <div className={"no-results-text"}>
+                    No se han encontrado resultados para su búsqueda
+                </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Companies;
